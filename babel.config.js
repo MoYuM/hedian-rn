6d25fa6b95a0ab53a980f8996c46e8f5538dd339
MoYuM <hanbaochang12138@gmail.com>
2025-09-09 20:45:26 +0800

    refactor: 去掉无用代码

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        '@babel/plugin-syntax-import-meta',
-        {
-          // 将 import.meta 转换为 process.env
-          module: 'process'
-        }
-      ]
-    ],
   };
 };
