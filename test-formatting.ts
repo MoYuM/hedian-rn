c42d0f2964615efe951159d263b780bc253f3213
MoYuM <hanbaochang12138@gmail.com>
2025-09-09 21:02:45 +0800

    测试 husky + lint-staged 格式化功能

---
+++ test-formatting.ts
@@ -0,0 +1,12 @@
+// 测试 husky + lint-staged 格式化
+const badFormat = {
+  name: 'test',
+  value: 123,
+  items: [1, 2, 3, 4, 5],
+};
+
+function badFunction(param1: string, param2: number) {
+  return param1 + param2;
+}
+
+export default badFormat;
